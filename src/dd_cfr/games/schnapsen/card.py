23899b94b907c22a6dca0c53724415228257d78f
David Toneian <david@toneian.com>
2023-05-27 19:18:05 +0200

    Simplify `Schnapsen.Card` by making it a `dataclass`.

--- src/dd_cfr/games/schnapsen/card.py
+++ src/dd_cfr/games/schnapsen/card.py
@@ -2,40 +2,17 @@
 Defines the :obj:`Card` class.
 """
 
+import dataclasses
+
 from .card_value import CardValue
 from .suit import Suit
 
 
+@dataclasses.dataclass
 class Card:
     """
     Represents a playing card.
     """
 
-    def __init__(self, suit: Suit, value: CardValue) -> None:
-        """
-        Construct a new instance.
-
-        :param suit: The suit of the card.
-        :param value: The card value.
-        """
-
-        self._suit = suit
-        self._value = value
-
-    def get_suit(self) -> Suit:
-        """
-        Return the card's suit.
-
-        :return: Returns the card's suit.
-        """
-
-        return self._suit
-
-    def get_card_value(self) -> CardValue:
-        """
-        Return the card value.
-
-        :return: Returns the card value.
-        """
-
-        return self._value
+    suit: Suit
+    card_value: CardValue
--- tests/games/schnapsen/test_card.py
+++ tests/games/schnapsen/test_card.py
@@ -3,10 +3,26 @@ from dd_cfr.games.schnapsen.card_value import CardValue
 from dd_cfr.games.schnapsen.suit import Suit
 
 
-def test_getters():
+def test_values():
     for suit in list(Suit):
         for value in list(CardValue):
             card = Card(suit, value)
 
-            assert card.get_suit() == suit
-            assert card.get_card_value() == value
+            assert card.suit == suit
+            assert card.card_value == value
+
+
+def test___eq__():
+    ace_of_spades = Card(Suit.SPADES, CardValue.ACE)
+    assert ace_of_spades == ace_of_spades
+
+    assert ace_of_spades == Card(Suit.SPADES, CardValue.ACE)
+
+    assert not ace_of_spades == (Suit.SPADES, CardValue.ACE)
+    assert ace_of_spades != (Suit.SPADES, CardValue.ACE)
+
+    assert not ace_of_spades == Card(Suit.SPADES, CardValue.KING)
+    assert ace_of_spades != Card(Suit.SPADES, CardValue.KING)
+
+    assert not ace_of_spades == (Suit.HEARTS, CardValue.ACE)
+    assert ace_of_spades != (Suit.HEARTS, CardValue.ACE)
